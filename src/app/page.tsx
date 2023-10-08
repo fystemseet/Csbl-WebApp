2ff7a85729fe305b7894e4807ee9dc625be34d36
FystemSeet <info@cusable.com>
2023-10-08 16:16:11 -0500

    move app to src directory for amplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* Header */}
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center items-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <h1 className="text-white text-4xl font-bold">Cusable</h1>
-        </div>
+      <div className="relative text-slate-700 font-semibold text-xl leading-6 dark:text-slate-200">
+        <h1>Cusable</h1>
       </div>
 
       {/* Content */}
@@ -15,9 +13,7 @@ export default function Home() {
 
       {/* Footer */}
       <div className="relative flex place-items-center">
-        <p>
-          The End.
-        </p>
+        The End.
       </div>
     </main>
   );
